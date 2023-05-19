7b5d139c4d7315ee86f5b3dba93b7571805def01
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-05-19 02:42:45 -0300

    Implemented joystick action

--- actions.py
+++ actions.py
@@ -1,8 +1,20 @@
-import pyautogui
+import platform
 import subprocess
 import time
 from abc import ABC, abstractmethod
 from enum import Enum
+
+import pyautogui
+
+
+PLATFORM = platform.system()
+
+if PLATFORM == "Windows":
+    from joystick_windows import Joystick
+elif PLATFORM == "Linux":
+    from joystick_linux import Joystick
+else:
+    raise ValueError(f"Unsuported platform: {PLATFORM}")
 
 
 class Action(ABC):
@@ -25,11 +37,13 @@ class Action(ABC):
         """
         # action being "linked" to the status of a particular control. For instance: a keys action
         # linked to a button, will press and hold down the keys while the button is down, and
-        # release them when the button is up.
+        # release them when the button is up. Or a midi controller linked to a joystick axis will
+        # move the axis of the joystick when the midi controller is moved
         LINKED_CONTROL_PRESS = "linked_press"
         LINKED_CONTROL_RELEASE = "linked_release"
-        # action being ran unlinked to the status of any control, for instance as a step in a
-        # script
+        LINKED_CONTROL_MOVE = "linked_move"
+        # action being ran unlinked to the status of any real life control, for instance as a step
+        # in a script
         UNLINKED = "unlinked"
 
     @abstractmethod
@@ -255,3 +269,106 @@ class RunCommand(Action):
         Read the config and return a configured Action.
         """
         return cls(raw_config)
+
+
+@Action.register
+class JoystickAction(Action):
+    """
+    Press specific buttons or move axes from a joystick.
+
+    Butons behave just like keys in both modes (linked and unlinked).
+    Axes in linked mode require a value to be passed in the call.
+    Axes in unlinked mode require a value to be specified in the config, initially, and will
+    allways be used.
+
+    Params:
+        - joystick_id (int): the number of virtual joystick to use
+        - control_type (JoystickAction.ControlType): the type of control to use, button or axis
+        - control_id (int): the number of button or axis to use
+        - unlinked_axis_value (optional float): the value to use when movin an axis in unlinked
+          mode (from -1 to 1)
+    """
+    PREFIX = "joystick"
+
+    JOYSTICKS_CACHE = {}
+
+    class ControlType(Enum):
+        """
+        Types of joystick controls.
+        """
+        AXIS = "axis"
+        BUTTON = "button"
+
+    def __init__(self, joystick_id, control_type, control_id, unlinked_axis_value=None):
+        self.joystick_id = joystick_id
+        self.control_type = control_type
+        self.control_id = control_id
+        self.unlinked_axis_value = unlinked_axis_value
+
+        if joystick_id not in self.JOYSTICKS_CACHE:
+            self.JOYSTICKS_CACHE[joystick_id] = Joystick(self.joystick_id)
+
+        self.joystick = self.JOYSTICKS_CACHE[joystick_id]
+
+        self.ensure_valid_controls()
+
+    def ensure_valid_controls(self):
+        """
+        Ensure that the specified keys are valid, otherwise raise an error.
+        """
+        if self.control_type == self.ControlType.BUTTON:
+            limit = len(self.joystick.BUTTONS) - 1
+        elif self.control_type == self.ControlType.AXIS:
+            limit = len(self.joystick.AXES) - 1
+
+        if self.control_id > limit:
+            raise ValueError(f"Joystick {self.control_type.value} goes form 0 to {limit}, "
+                             f"can't be {self.control_id}")
+
+    def run(self, mode, value=None):
+        """
+        Execute the acton.
+        """
+        if self.control_type == self.ControlType.BUTTON:
+            if mode == self.Mode.UNLINKED:
+                self.joystick.press_button(self.control_id)
+                time.sleep(0.1)
+                self.joystick.release_button(self.control_id)
+            elif mode == self.Mode.LINKED_CONTROL_PRESS:
+                self.joystick.press_button(self.control_id)
+            elif mode == self.Mode.LINKED_CONTROL_RELEASE:
+                self.joystick.release_button(self.control_id)
+
+        elif self.control_type == self.ControlType.AXIS:
+            if value and self.unlinked_axis_value is None:
+                raise ValueError("To use an axis not tied to an actual midi device axis, you must "
+                                 f"specify a fixed value for it (joystick {self.joystick_id}, "
+                                 f"axis {self.control_id})")
+            if value is None:
+                value = self.unlinked_axis_value
+
+            if mode in (self.Mode.UNLINKED, self.Mode.LINKED_CONTROL_MOVE):
+                self.joystick.move_axis(self.control_id, value)
+
+    @classmethod
+    def deserialize(cls, raw_config):
+        """
+        Read the config and return a configured Action.
+        """
+        try:
+            parts = raw_config.split()
+            assert len(parts) in (3, 5)
+
+            joystick_id = int(parts[0])
+            control_type = cls.ControlType(parts[1])
+            control_id = int(parts[2])
+
+            if len(parts) == 5:
+                assert parts[3] == "value"
+                unlinked_axis_value = float(parts[4])
+            else:
+                unlinked_axis_value = None
+        except:
+            raise ValueError(f"The format of a 'joystick' action is incorrect: {raw_config}")
+
+        return cls(joystick_id, control_type, control_id, unlinked_axis_value)
