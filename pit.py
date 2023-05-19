2798c7f7aaa972865ae31bf0c06b05dfc89c8564
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-05-19 20:54:05 -0300

    Organized code, and better grid defaults

--- pit.py
+++ pit.py
@@ -4,8 +4,47 @@ import yaml
 
 from actions import Action
 
-DEFAULT_GRID_WIDTH = 16
-DEFAULT_GRID_HEIGHT = 4
+DEFAULT_GRID_WIDTH = 20
+DEFAULT_GRID_HEIGHT = 10
+
+
+class Page:
+    """
+    A collection of controls to show together, mapped to actions.
+    """
+    def __init__(self, name, background, controls,
+                 width=DEFAULT_GRID_WIDTH, height=DEFAULT_GRID_HEIGHT):
+        self.name = name
+        self.background = background
+        self.width = width
+        self.height = height
+        self.controls = controls
+
+    @classmethod
+    def read(cls, name, pages_path):
+        """
+        Read the page definition from a yaml file.
+        """
+        page_path = pages_path / (name + ".page")
+        with open(page_path, "r") as page_file:
+            raw_config = yaml.safe_load(page_file)
+
+        raw_config["name"] = name
+        raw_config["controls"] = [
+            PageButton.deserialize(ctrl_raw_config)
+            for ctrl_raw_config in raw_config["controls"]
+        ]
+        return cls(**raw_config)
+
+    @classmethod
+    def available_pages(cls, pages_path):
+        """
+        List all the available (config files) pages.
+        """
+        return [
+            page_path.name[:-5]
+            for page_path in pages_path.glob("*.page")
+        ]
 
 
 class PageButton:
@@ -93,42 +132,3 @@ class PageButton:
         return cls(x=x, y=y, width=width, height=height,
                    linked_action=linked_action, script=script,
                    **raw_config)
-
-
-class Page:
-    """
-    A collection of controls to show together, mapped to actions.
-    """
-    def __init__(self, name, background, controls,
-                 width=DEFAULT_GRID_WIDTH, height=DEFAULT_GRID_HEIGHT):
-        self.name = name
-        self.background = background
-        self.width = width
-        self.height = height
-        self.controls = controls
-
-    @classmethod
-    def read(cls, name, pages_path):
-        """
-        Read the page definition from a yaml file.
-        """
-        page_path = pages_path / (name + ".page")
-        with open(page_path, "r") as page_file:
-            raw_config = yaml.safe_load(page_file)
-
-        raw_config["name"] = name
-        raw_config["controls"] = [
-            PageButton.deserialize(ctrl_raw_config)
-            for ctrl_raw_config in raw_config["controls"]
-        ]
-        return cls(**raw_config)
-
-    @classmethod
-    def available_pages(cls, pages_path):
-        """
-        List all the available (config files) pages.
-        """
-        return [
-            page_path.name[:-5]
-            for page_path in pages_path.glob("*.page")
-        ]
