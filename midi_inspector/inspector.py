f64c5b2d8036354e3a57d080fd5f256d5f466eb5
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-05-19 23:35:28 -0300

    Added basic midi inspector tool

---
+++ midi_inspector/inspector.py
@@ -0,0 +1,56 @@
+from time import sleep
+import sys
+import platform
+
+import pygame
+import pygame.midi as pgm
+import mido
+
+
+def run_inspector(device_names):
+    """
+    Run the main loop of the app.
+    """
+    pygame.init()
+    pgm.init()
+
+    if platform.system() == "Windows":
+        midi_backend = mido.Backend('mido.backends.pygame')
+    else:
+        midi_backend = mido.Backend('mido.backends.rtmidi')
+
+    ports = [midi_backend.open_input(name) for name in device_names]
+
+    try:
+        while True:
+            for port, message in mido.ports.multi_receive(ports, yield_ports=True):
+                if message.type == "program_change":
+                    kind = "is_program"
+                    id_ = "(not applicable)"
+                    value = message.program
+                elif message.type == "control_change":
+                    kind = "control"
+                    id_ = message.control
+                    value = message.value
+                elif message.type == "note_on":
+                    kind = "note"
+                    id_ = message.note
+                    value = 1
+                elif message.type == "note_off":
+                    kind = "note"
+                    id_ = message.note
+                    value = 0
+
+                print(
+                    f'device="{port.name}"',
+                    f"kind={kind}", f"control/note={id_}", f"value={value}",
+                    flush=True,
+                )
+
+            sleep(0.01)
+    except KeyboardInterrupt:
+        print("Quitting", flush=True)
+        pgm.quit()
+
+
+run_inspector(sys.argv[1:])
